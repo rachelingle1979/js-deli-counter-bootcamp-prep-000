a41a7601ee4d2b2a95e8c2f863a41cf91884d9fb
Rachel Ingle <rachelpost79@yahoo.com>
2018-01-30 20:55:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function currentLine(katzDeliLine){
   }
   
   for (let i = 0; i < katzDeliLine.length; i++){
-    lineList.push(i+1 + '. ' + katzDeliLine[i]);
+    lineList.push(i+1 + '. ' + katzDeliLine[i] + ',');
   }
 
   return lineList.join(' ');
