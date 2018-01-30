a71fb9b0581ec1aa68c12513d187bcaecb7bd9d4
Rachel Ingle <rachelpost79@yahoo.com>
2018-01-30 20:50:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 var katzDeli = [];
 
 function currentLine(katzDeliLine){
-  let lineList = ['The line is currently'];
+  let lineList = ['The line is currently:'];
   if (katzDeliLine.length === 0){
 	  lineList.push(' empty.');
   }
   
   for (let i = 0; i < katzDeliLine.length; i++){
-    lineList.push(":" + i+1 + '. ' + katzDeliLine[i]);
+    lineList.push(i+1 + '. ' + katzDeliLine[i]);
   }
 
   return lineList.join(' ');
 }
-
-var katzDeliLine = [];
+var yourMom = "Sara";
+var katzDeliLine = ['Pam', 'Jessica', yourMom];
 console.log(currentLine(katzDeliLine));
 
-function takeANumber(line, name)
+var next = "Rachel";
+
+function takeANumber(line, name){
+  katzDeliLine.push(name);
+  return name + "is now" + katzDeliLine.length + "in line";
+}
+
+
+
+
+
+
+
+
+
+
