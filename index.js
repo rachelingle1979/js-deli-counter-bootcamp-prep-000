5342667ef7f2046cd3c96a6ca67b731442312700
Rachel Ingle <rachelpost79@yahoo.com>
2018-01-30 20:30:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var katzDeli = [];
 
 function currentLine(katzDeliLine){
-  let lineList = 'The line is currently';
+  let lineList = 'The line is currently: ';
   for (let i = 0; i < katzDeliLine.length; i++){
-    lineList.concat(indexOf(katzDeliLine[i]), '.', )
+    lineList.concat(indexOf(katzDeliLine[i]), '. ', katzDeliLine[i])
   }
+  return lineList;
 }
 
 function takeANumber(line, name)
