b0d21f85fcfe1daa9d393d630c1726c7d472ff13
Rachel Ingle <rachelpost79@yahoo.com>
2018-01-30 20:25:22 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+var katzDeli = [];
+
+function currentLine(katzDeliLine){
+  let lineList = '';
+  for (let i = 0; i < katzDeliLine.length; i++){
+    line
+  }
+}
+
+function takeANumber(line, name)
